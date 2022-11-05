cbc4b09adac7af8221d4cb0340e71cede2b3c33d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-11-05 17:05:55 +0600

    register location notification

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Shah Md Imran Hossain on 5/11/22.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +13,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Regiser", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+        
     }
 
-
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh!")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        // what to be shown
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird cathes the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["CustomData" : "fizzbuzz"]
+        content.sound = .default
+        
+        // when to be shown
+        var dateComponent = DateComponents()
+        dateComponent.hour = 10
+        dateComponent.minute = 30
+        
+        // no time out
+        // true if you want to repeat the notication everyday
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
+        
+        // time wait before notification
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // uuid for unique user id
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
 }
 
